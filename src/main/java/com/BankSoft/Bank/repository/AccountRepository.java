4f5176c4fca1c40ae32e4ebbb07e8f88418957f1
isaacmoura23 <zackisaacdev@gmail.com>
2026-07-01 01:34:49 -0300

    feat(repository): create jpa repositories

--- src/main/java/com/BankSoft/Bank/repository/AccountRepository.java
+++ src/main/java/com/BankSoft/Bank/repository/AccountRepository.java
@@ -2,7 +2,6 @@ package com.BankSoft.Bank.repository;
 
 import com.BankSoft.Bank.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
